6c5c5525b13c49aabe03058f46b7f3391d9d7ea0
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-21 15:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const byteSize = (str) => {
-  // write your code here
-};
+function byteSize(str) {
+  // Create a Blob object with the input string
+  const blob = new Blob([str]);
+
+  // Retrieve the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
+}
+
+// Test cases
+console.log(byteSize('hello world'));      // Output: 11
+console.log(byteSize('안녕하세요'));         // Output: 15
+console.log(byteSize(''));                  // Output: 0
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
